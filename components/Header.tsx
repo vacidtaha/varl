92db293d0172eb6269289df7257f6d0c617fbec2
Taha Vacid <tahavacid@Taha-MacBook-Pro.local>
2025-12-26 20:46:41 +0300

    Favicon güncellendi, kullanılmayan dosyalar silindi, kayan metin kaldırıldı

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import { LightBoard } from "./LightBoard";
 
 export default function Header() {
   return (
@@ -9,23 +8,11 @@ export default function Header() {
       {/* Logo - Ortada */}
       <div className="absolute inset-0 flex items-center justify-center">
         <Image
-          src="/varlogo.png"
+          src="/vacid-2.png"
           alt="Vacid Logo"
           width={600}
           height={200}
           priority
-        />
-      </div>
-      
-      {/* LightBoard - Alt kısımda tam genişlik (sadece masaüstü) */}
-      <div className="hidden md:block absolute bottom-0 left-0 w-[100vw] pointer-events-auto">
-        <LightBoard
-          text="AUT VIAM INVENIAM AUT FACIAM"
-          rows={20}
-          lightSize={4}
-          gap={2}
-          updateInterval={150}
-          disableDrawing={true}
         />
       </div>
     </header>
